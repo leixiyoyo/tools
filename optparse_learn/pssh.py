923610911229aaad7171885fb979b163f538901d
lickeryy <1721186546@qq.com>
2020-09-18 12:06:10 +0800

    add version and help

--- optparse_learn/pssh.py
+++ optparse_learn/pssh.py
@@ -3,16 +3,17 @@
 #Author: Leiyangfeng
 import  os,optparse,argparse,paramiko
 def input_args():
-    hstr = '%prog -i host or host_file -u user -p password -P port'
-    parser = argparse.ArgumentParser(prog='%prog',usage=hstr,description='pssh') #指定版本,通过scripts.py --version查看
+    # hstr = '%prog -i host or host_file -u user -p password -P port'
+    parser = argparse.ArgumentParser() #指定版本,通过scripts.py --version查看
     # print parser.usage
     # # print parser.option_list
     # # print parser.option_class
-    parser.add_argument('-i', '--host', dest='hostname', default='127.0.0.1', help='target host or host_file')
+    parser.add_argument('-i', '--host', dest='hostname', default='192.168.66.100', help='target host or host_file')
     parser.add_argument('-u', '--user', dest='username', default='root', help='username')
-    parser.add_argument('-p', '--password', dest='password', default=None, help='password')
+    parser.add_argument('-p', '--password', dest='password', default='123456', help='password')
     parser.add_argument('-P', '--port', dest='port',default=22, help='ssh_port of target host')
-    
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s1.0',help='Display version') #%(prog)s获取脚本名
+
 
     return parser
 
@@ -30,11 +31,14 @@ if __name__ == '__main__':
     parser=input_args()
     opt,arg_list=parser.parse_known_args() # arg_list为列表
     # client=cct(**opt) # opt是个instance相当于对象,看着跟字典一样,所以不能这样打散
-    print opt,type(opt),arg_list,type(arg_list)
+    # print opt,type(opt),arg_list,type(arg_list)
     cmd='' # arg_list是一个列表,需要拼接成字符串,当做命令串给client处理
     for i in arg_list:
         cmd+='%s '%i
+    # cmd='ls -l'
     client=cct(hostname=opt.hostname,username=opt.username,password=opt.password,port=opt.port)
+    # client=cct(hostname='192.168.66.100',username=opt.username,password='123456',port=opt.port)
+    # client=cct(hostname='192.168.66.100',username='root',password='123456',port=22)
     stdin, stdout, stderr = client.exec_command(cmd)
     if stderr:
         print stderr.read().decode('utf-8')
